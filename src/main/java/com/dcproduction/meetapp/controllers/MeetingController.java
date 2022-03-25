49309ecbb21746622c65fd9a6b17ac9d8bfa9a0a
denalichin <denalichin@gmail.com>
2022-03-25 14:15:20 -0700

    implemented query to omit password, still not implemented in service layer yet

--- src/main/java/com/dcproduction/meetapp/controllers/MeetingController.java
+++ src/main/java/com/dcproduction/meetapp/controllers/MeetingController.java
@@ -3,9 +3,11 @@ package com.dcproduction.meetapp.controllers;
 import java.util.List;
 
 import com.dcproduction.meetapp.classes.Meeting;
+import com.dcproduction.meetapp.classes.User;
 import com.dcproduction.meetapp.repositories.MeetingRepository;
 import com.dcproduction.meetapp.services.MeetingService;
 
+import org.bson.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +24,9 @@ public class MeetingController {
     
     @Autowired
     MeetingService service;
-    // MeetingRepository meetingDB;
+
+    @Autowired
+    MeetingRepository meetingDB;
 
     @GetMapping
     public List<Meeting> fetchAllMeetings() {
@@ -33,9 +37,33 @@ public class MeetingController {
     
     @GetMapping("/findmeeting")
     public Meeting findMeeting() {
-        System.out.println("Finding meeting");
+        System.out.println("FINDING MEETING");
         return service.findMeeting("test meeting");
         // return this.meetingDB.findAll();
     }
 
+
+    // @GetMapping("/finduser") //TEMPORARY make this call from service, not from db
+    // String findUser() {
+    //     System.out.println("FINDING USER");
+    //     return meetingDB.findUsers("test meeting");
+    //     // return this.meetingDB.findAll();
+    // }
+
+    @GetMapping("/finduser") //TEMPORARY make this call from service, not from db
+    String findUser() { 
+        System.out.println("FINDING USER");
+
+        return meetingDB.findUsers("test meeting");
+        // return this.meetingDB.findAll();
+    }
+
+ 
+    
+    // @GetMapping("/finduser")
+    // public List<User> findUser() {
+    //     System.out.println("FINDING USER");
+    //     return service.findUsers("test meeting");
+    //     // return this.meetingDB.findAll();
+    // }
 }
--- src/main/java/com/dcproduction/meetapp/repositories/MeetingRepository.java
+++ src/main/java/com/dcproduction/meetapp/repositories/MeetingRepository.java
@@ -1,6 +1,11 @@
 package com.dcproduction.meetapp.repositories;
 
+import java.util.List;
+
 import com.dcproduction.meetapp.classes.Meeting;
+import com.dcproduction.meetapp.classes.User;
+
+import org.bson.json.JsonObject;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Component;
@@ -14,12 +19,22 @@ import org.springframework.stereotype.Repository;
 public interface MeetingRepository extends MongoRepository<Meeting, String> {
 
     @Query("{name:'?0'}")
-    Meeting findMeeting(String name); //we could name this function anything
+    Meeting findMeeting(String meetingName); //we could name this function anything
     // public Meeting getById(String id);
+   
+    // @Query(value="{name: '?0' }")
+    @Query(value="{ name : '?0'}", fields="{users : { password : 0}}")
+    //"{ user : { name : 1 } }")
+    String findUsers(String meetingName);
 
     //findById may return a null, while getById always returns an object, even if entry doesn't exist
 
     // @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
     // List<GroceryItem> findAll(String category);
 
+
+    // template.query(Person.class).distinct("lastname").all();   
+//     List<Person> result = template.query(Person.class)
+//   .matching(query(where("age").lt(50).and("accounts.balance").gt(1000.00d)))
+//   .all();
 }
